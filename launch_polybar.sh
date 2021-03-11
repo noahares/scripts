189e1526476a4cd59c818e69f927cd4e28260287
Noah Wahl <noah.ares@yahoo.de>
2021-03-11 19:39:27 +0100

    Update Polybar

--- launch_polybar.sh
+++ launch_polybar.sh
@@ -2,6 +2,12 @@
 
 BAR=""
 
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pidof polybar >/dev/null; do sleep 1; done
+
 case $(hostname) in
   'arch' )
     BAR='desktop';;
@@ -11,10 +17,20 @@ case $(hostname) in
     BAR='laptop';;
 esac
 
-# Terminate already running bar instances
-killall -q polybar
+if [[ $(polybar -m | wc -l) > 1 ]]; then
+  M1=$(polybar -m | awk -F: 'NR==1{print $1}')
+  M2=$(polybar -m | awk -F: 'NR==2{print $1}')
+  MONITOR=$M1 polybar -rq wm &
+  MONITOR=$M2 polybar -rq wm &
+  MONITOR=$M2 polybar -rq media &
+  MONITOR=$M2 polybar -rq state &
+  MONITOR=$M2 polybar -rq tray &
+else
+  M1=$(polybar -m | awk -F: 'NR==1{print $1}')
+  MONITOR=$M1 polybar -rq wm &
+  MONITOR=$M1 polybar -rq media &
+  MONITOR=$M1 polybar -rq state &
+  MONITOR=$M1 polybar -rq tray &
+fi
 
-# Wait until the processes have been shut down
-while pidof polybar >/dev/null; do sleep 1; done
-
-for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar $BAR & done
+#for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar $BAR & done
