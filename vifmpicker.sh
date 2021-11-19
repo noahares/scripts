50129fdce182ff9a283f567ceb0e5f9b237b51c8
Noah Wahl <noah.ares@yahoo.de>
2021-11-19 15:31:19 +0100

    Improve mutt file picker

--- vifmpicker.sh
+++ vifmpicker.sh
@@ -1,6 +1,8 @@
 tmpfile=/tmp/vifmpick
 if \[ -z "$1" \]; then
-   vifm --choose-files $tmpfile && echo "$(awk 'BEGIN {printf "%s", "push "} {printf "%s", "<attach-file>"$0"<enter>"}'  $tmpfile)" > $tmpfile
+    vifm --choose-files $tmpfile &&
+    sed -i 's/ /^V /g' $tmpfile &&
+    echo "$(awk 'BEGIN {printf "%s", "push "} {printf "%s", "<attach-file>\""$0"\"<enter>"}'  $tmpfile)" > $tmpfile
 elif \[ $1 == "clean" \]; then
-  rm $tmpfile
+    rm $tmpfile
 fi
