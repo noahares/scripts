43c4ec088b17b8ece04de6d2f0b780c0e2ea0823
Noah Wahl <noah.ares@yahoo.de>
2021-11-20 11:41:26 +0100

    Update mailbox and brightness

--- brightness.sh
+++ brightness.sh
@@ -2,8 +2,12 @@
 CURRBRIGHT=$(xrandr --current --verbose | grep -m 1 'Brightness:' | cut -f2- -d:)
 if [ "$1" = "+" ] && [ $(echo "$CURRBRIGHT < 1" | bc) -eq 1 ]
 then
-xrandr --output eDP1 --brightness $(echo "$CURRBRIGHT + 0.1" | bc)
+NEWBRIGHTNESS=$(echo "$CURRBRIGHT + 0.1" | bc)
+echo $NEWBRIGHTNESS > $XDG_DATA_HOME/brightness
+xrandr --output eDP1 --brightness $NEWBRIGHTNESS
 elif [ "$1" = "-" ] && [ $(echo "$CURRBRIGHT > 0" | bc) -eq 1 ]
 then
-xrandr --output eDP1 --brightness $(echo "$CURRBRIGHT - 0.1" | bc)
+NEWBRIGHTNESS=$(echo "$CURRBRIGHT - 0.1" | bc)
+echo $NEWBRIGHTNESS > $XDG_DATA_HOME/brightness
+xrandr --output eDP1 --brightness $NEWBRIGHTNESS
 fi
