93dc91bfac96305f89d0fd8c7761b794f18600dc
Noah Wahl <noah.ares@yahoo.de>
2021-12-11 12:27:57 +0100

    Switch between headphones and lineout

--- output_switch.sh
+++ output_switch.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-#cur_sink=$(pacmd list-sinks | grep '* index:' | tr -d '\n' | tail -c 1)
-new_sink=$(pacmd list-sinks | grep 'index:' | grep -v '*' | tr -d '\n' | tail -c 2)
-
-pacmd set-default-sink $new_sink
-
-pacmd list-sink-inputs | grep index | while read line
-do
-        pacmd move-sink-input `echo $line | cut -f2 -d ' '` $new_sink
-done
-
-polybar-msg hook audio_out 1
+if pactl list sinks | grep 'Active Port' | grep -q headphones; then
+  pactl set-sink-port 0 analog-output-lineout
+else
+  pactl set-sink-port 0 analog-output-headphones
+fi
