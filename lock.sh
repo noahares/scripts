b0e1bf55a73648c9167d323a6fe8f64c55512828
Noah Wahl <noah.ares@yahoo.de>
2023-01-12 15:20:55 +0100

    Update lock

--- lock.sh
+++ lock.sh
@@ -2,6 +2,6 @@
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 [[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
-mpc pause
+playerctl pause
 i3lock -u -i /tmp/screen.png
 rm /tmp/screen.png
